acee8b345724a42111f633d96a0c71ac3e4f5f50
anamerasul <anamerasul@gmail.com>
2022-05-09 17:13:54 +0600

    speific orders for user route add

--- index.js
+++ index.js
@@ -36,9 +36,16 @@ const run = async () => {
     const fruitsCollection = client
       .db("allfruitsCollection")
       .collection("allfruits");
+    const orderCollection = client
+      .db("allfruitsCollection")
+      .collection("allfruitsorder");
     const fruitsbannerCollection = client
       .db("allfruitsCollection")
       .collection("allfruitsbanner");
+
+    const fruitsblogsCollection = client
+      .db("allfruitsCollection")
+      .collection("allfruitsblogs");
     console.log(`connection too allfruitsCollection`);
 
     // jwt token in login
@@ -71,6 +78,15 @@ const run = async () => {
       res.send(banners);
     });
 
+    app.get("/blogs", async (req, res) => {
+      const query = {};
+      const cursor = fruitsblogsCollection.find(query);
+
+      const blogs = await cursor.toArray();
+
+      res.send(blogs);
+    });
+
     // get invebtory data
 
     app.get("/inventory", async (req, res) => {
@@ -87,6 +103,37 @@ const run = async () => {
 
       const item = await fruitsCollection.findOne(query);
       res.send(item);
+    });
+
+    app.put("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateInventory = req.body;
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+      console.log(email);
+      const decoded = verifyJwtToken(accessToken, process.env.ACCESS_TOKEN);
+      console.log(decoded);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          DeliverdQuantiy: updateInventory.DeliverdQuantiy + 1,
+          StockQuantity: updateInventory.StockQuantity - 1,
+        },
+      };
+
+      if (email === decoded.email) {
+        const result = await fruitsCollection.updateOne(
+          filter,
+          updatedDoc,
+          options
+        );
+        res.send({ success: "Product Update successfully" });
+        // res.send(result);
+      } else {
+        // res.send({ success: "UnAuthoraized Access" });
+        res.status(403).send({ message: "forbidden access" });
+      }
     });
 
     app.get("/inventory-count", async (req, res) => {
@@ -112,14 +159,252 @@ const run = async () => {
         res.send({ success: "Product Upload TO INventory successfully" });
         // res.send(result);
       } else {
-        res.send({ success: "UnAuthoraized Access" });
+        // res.send({ success: "UnAuthoraized Access" });
+        res.status(403).send({ message: "forbidden access" });
       }
       // const result = await productCollection.insertOne(product);
 
       // res.send(result);
     });
 
-    app.post("/products", (req, res) => {});
+    app.get("/allorders", async (req, res) => {
+      const query = {};
+      const cursor = orderCollection.find(query);
+
+      const items = await cursor.toArray();
+      res.send(items);
+      // } else {
+      //   res.send({ success: "UnAuthoraized Access" });
+      // }
+    });
+
+    // get order spefic user
+
+    app.get("/orders", async (req, res) => {
+      const tokenInfo = req.headers.authorization;
+
+      console.log(tokenInfo);
+      const [email, accessToken] = tokenInfo.split(" ");
+      // console.log(email, accessToken)
+
+      const decoded = verifyJwtToken(accessToken);
+
+      if (email === decoded.email) {
+        const orders = await orderCollection.find({ email: email }).toArray();
+        res.send(orders);
+      } else {
+        res.send({ success: "UnAuthoraized Access" });
+      }
+    });
+    // app.get("/allorders:/id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+
+    //   const item = await orderCollection.findOne(query);
+    //   res.send(item);
+    // });
+
+    app.get("/allorders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+
+      const item = await orderCollection.findOne();
+      res.send(item);
+    });
+
+    // deleteorder
+
+    app.delete("/allorders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+      const decoded = verifyJwtToken(accessToken, process.env.ACCESS_TOKEN);
+      if (email === decoded.email) {
+        const result = await orderCollection.deleteOne(query);
+        res.send({ success: "Product Delete successfully" });
+        // res.send(result);
+      } else {
+        // res.send({ success: "UnAuthoraized Access" });
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    // app.get("/all/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+
+    //   const item = await orderCollection.findOne(query);
+    //   res.send(item);
+    // });
+
+    // app.get("/orders/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+
+    //   const item = await orderCollection.findOne(query);
+    //   res.send(item);
+    // });
+    app.post("/orders", async (req, res) => {
+      // // const orderInfo = req.body;
+      // console.log(orderInfo);
+      // const result = await orderCollection.insertOne(orderInfo);
+      // res.send({ success: "Product Upload Successfully" });
+      // } else {
+      //   res.send({ success: "UnAuthoraized Access" });
+      // }
+
+      const orderInfo = req.body;
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+      console.log(email);
+      const decoded = verifyJwtToken(accessToken, process.env.ACCESS_TOKEN);
+      console.log(decoded);
+
+      // const decoded = verifyToken(accessToken);
+
+      console.log(decoded, decoded.email);
+
+      if (email === decoded.email) {
+        const result = await orderCollection.insertOne(orderInfo);
+        res.send({ success: "Product ADD TO USER successfully" });
+        // res.send(result);
+      } else {
+        // res.send({ success: "UnAuthoraized Access" });
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateInventory = req.body;
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+      console.log(email);
+      const decoded = verifyJwtToken(accessToken, process.env.ACCESS_TOKEN);
+      console.log(decoded);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          myquantity: updateInventory.myquantity,
+        },
+      };
+
+      if (email === decoded.email) {
+        const result = await orderCollection.updateOne(
+          filter,
+          updatedDoc,
+          options
+        );
+        res.send({ success: "Product Update successfully" });
+        // res.send(result);
+      } else {
+        // res.send({ success: "UnAuthoraized Access" });
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    app.get("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+
+      const item = await fruitsCollection.findOne(query);
+      res.send(item);
+    });
+
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateInventory = req.body;
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+      console.log(email);
+      const decoded = verifyJwtToken(accessToken, process.env.ACCESS_TOKEN);
+      console.log(decoded);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          // StockQuantity: updateInventory.newStockQuantity,
+          StockQuantity: updateInventory.StockQuantity + 1,
+        },
+      };
+
+      if (email === decoded.email) {
+        const result = await fruitsCollection.updateOne(
+          filter,
+          updatedDoc,
+          options
+        );
+        res.send({ success: "Product Update successfully" });
+        // res.send(result);
+      } else {
+        // res.send({ success: "UnAuthoraized Access" });
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    // delete products
+
+    app.delete("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const Jwttokeninfo = req.headers.authorization;
+      const [email, accessToken] = Jwttokeninfo.split(" ");
+      const decoded = verifyJwtToken(accessToken, process.env.ACCESS_TOKEN);
+      if (email === decoded.email) {
+        const result = await fruitsCollection.deleteOne(query);
+        res.send({ success: "Product Delete successfully" });
+        // res.send(result);
+      } else {
+        // res.send({ success: "UnAuthoraized Access" });
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
+    // app.post("/products", (req, res) => {});
+
+    app.get("/manageinventory", async (req, res) => {
+      const query = req.body;
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+
+      console.log(query);
+
+      console.log(req.query);
+      const cursor = fruitsCollection.find(query);
+
+      let inventories;
+
+      if (page || size) {
+        inventories = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        inventories = await cursor.toArray();
+      }
+
+      res.send(inventories);
+    });
+
+    app.get("/inventory-count", async (req, res) => {
+      const count = await fruitsCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+
+    // use post product by id
+
+    app.post("/inventoryByKeys", async (req, res) => {
+      const keys = req.body;
+      const ids = keys.map((id) => ObjectId(id));
+      const query = { _id: { $in: ids } };
+      const cursor = fruitsCollection.find(query);
+
+      const inventories = await cursor.toArray();
+      console.log(keys);
+
+      res.send(inventories);
+    });
   } finally {
     //
   }
@@ -149,7 +434,24 @@ const verifyJwtToken = (token) => {
 
 // running server on browser
 app.get("/", (req, res) => {
-  res.send("fruits-hub server is running and waiting for data");
+  res.send(`
+  <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>FRUITS HUB </title>
+  </head>
+  <body>
+    <h1 class="text-primary text-center my-5 py-5">WELCOME TO FRUITS HUB SERVER</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
+  
+  `);
 });
 
 app.listen(port, () => {
